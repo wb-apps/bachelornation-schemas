81c30d3b3ed92e9273b8dc1c0a9b0ac1397db123
Albert <albertgcsula@gmail.com>
2020-03-30 17:05:54 -0700

    v0.2.6
    * Use `gdbots/schemas` v1.6.5.
    * Use `triniti/schemas` v1.1.8.
    * __Add Schemas:__
      * `bachelornation:canvas:block:tumblr-post-block`

---
+++ build/php/src/Bachelornation/Schemas/Canvas/Block/TumblrPostBlock.php
@@ -0,0 +1,9 @@
+<?php
+// @link https://schemas.bachelornation.com/json-schema/bachelornation/canvas/block/tumblr-post-block/latest.json#
+namespace Bachelornation\Schemas\Canvas\Block;
+
+use Gdbots\Pbj\Message;
+
+interface TumblrPostBlock extends Message
+{
+}
